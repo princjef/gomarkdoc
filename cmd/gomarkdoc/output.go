princjef/gomarkdoc

commit 2efd0c5dad2720812c7adc708560fe69f04e95fb

Generated files have an execution permissions on Linux (#72)

cmd/gomarkdoc/output.go
@@ -82,7 +82,7 @@ func writeFile(fileName string, text string) error {
 	folder := filepath.Dir(fileName)
 
 	if folder != "" {
-		if err := os.MkdirAll(folder, 0755); err != nil {
+		if err := os.MkdirAll(folder, 0664); err != nil {
 			return fmt.Errorf("failed to create folder %s: %w", folder, err)
 		}
 	}

.gitignore
@@ -2,3 +2,11 @@
 /gomarkdoc
 /bin/
 README-test.md
+
+# editor and IDE paraphernalia
+.vscode/*
+.idea
+*.iml
+*.swp
+*.swo
+*~

commit e40eded375da25ccabe70515f2cd373201f38455

Fix incorrect folder and file persmissions introduced by the #72

cmd/gomarkdoc/output.go
@@ -82,12 +82,12 @@ func writeFile(fileName string, text string) error {
 	folder := filepath.Dir(fileName)
 
 	if folder != "" {
-		if err := os.MkdirAll(folder, 0664); err != nil {
+		if err := os.MkdirAll(folder, 0755); err != nil {
 			return fmt.Errorf("failed to create folder %s: %w", folder, err)
 		}
 	}
 
-	if err := ioutil.WriteFile(fileName, []byte(text), 0755); err != nil {
+	if err := ioutil.WriteFile(fileName, []byte(text), 0664); err != nil {
 		return fmt.Errorf("failed to write file %s: %w", fileName, err)
 	}
 

magefile.go
@@ -29,6 +29,10 @@ func Generate() error {
 	return shellcmd.Command(`go generate .`).Run()
 }
 
+func Build() error {
+	return shellcmd.Command(`go build -o ./bin/gomarkdoc ./cmd/gomarkdoc`).Run()
+}
+
 func Doc() error {
 	return shellcmd.RunAll(
 		`go run ./cmd/gomarkdoc .`,

cmd/gomarkdoc/command_test.go
@@ -298,7 +298,7 @@ func TestCommand_embed(t *testing.T) {
 	data, err := os.ReadFile("./embed/README-template.md")
 	is.NoErr(err)
 
-	err = os.WriteFile("./embed/README-test.md", data, 0644)
+	err = os.WriteFile("./embed/README-test.md", data, 0664)
 	is.NoErr(err)
 
 	main()