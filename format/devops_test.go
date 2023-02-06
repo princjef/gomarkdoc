1144ecad54a5d1ee26f8a59ec2b312cf5e892532
Jeff Principe <princjef@gmail.com>
2023-02-05 16:11:43 -0800

    chore(deps): update to go 1.20 and latest golangci-lint (#85)

--- format/devops_test.go
+++ format/devops_test.go
@@ -97,9 +97,9 @@ func TestRawHeader(t *testing.T) {
 
 func TestLocalHref(t *testing.T) {
 	tests := map[string]string{
-		"Normal Header":       "#normal-header",
-		" Leading whitespace": "#leading-whitespace",
-		"Multiple	 whitespace": "#multiple--whitespace",
+		"Normal Header":          "#normal-header",
+		" Leading whitespace":    "#leading-whitespace",
+		"Multiple	 whitespace":   "#multiple--whitespace",
 		"Special(#)%^Characters": "#special%28%23%29%25%5Echaracters",
 		"With:colon":             "#with%3Acolon",
 	}
